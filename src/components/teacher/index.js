951e9bf81a23b942887d0c1d1083c32cfa150712
shavkatovivich <frontend6774@gmail.com>
2022-12-08 16:52:25 +0000

    .

--- src/components/teacher/index.js
+++ src/components/teacher/index.js
@@ -1,17 +1,56 @@
-import React from "react";
-import { NavLink } from "react-router-dom";
-import './teacher.scss'
-const Teacher = () => {
-    return(
-        <>
-            <div className="d-flex justify-content-between">
-                <h1>O'qituvchilar ro'yhati</h1>
-                <NavLink to="/teacher" className='teacher' activeclassname='active'>
-                    O'qituvchi qo'shish                  
-                </NavLink>
-            </div>
-        </>
-    )
-}
+import React, { useState } from "react";
+import "./teacher.scss";
+import { Button, Modal } from "antd";
+// import 'antd/dist/antd.css';
 
-export default Teacher
+import { NavLink } from "react-router-dom";
+const Teacher = () => {
+  const [open, setOpen] = useState(false);
+  const [confirmLoading, setConfirmLoading] = useState(false);
+  const [modalText, setModalText] = useState("Content of the modal");
+  const showModal = () => {
+    setOpen(true);
+  };
+
+  const handleOk = () => {
+    setModalText("The modal will be closed after two seconds");
+    setConfirmLoading(true);
+    setTimeout(() => {
+      setOpen(false);
+      setConfirmLoading(false);
+    }, 2000);
+  };
+
+  const handleCancel = () => {
+    console.log("Clicked cancel button");
+    setOpen(false);
+  };
+
+  return (
+    <>
+      <div className="d-flex justify-content-between">
+        <h1>O'qituvchilar ro'yhati</h1>
+        <NavLink to="/teacher" className="teacher" activeclassname="active" onClick={showModal} >
+          O'qituvchi qo'shish
+        </NavLink>
+
+        <Modal className="modal"
+        //   title="O'qituvchi qo'shing"
+          open={open}
+          onOk={handleOk}
+          confirmLoading={confirmLoading}
+          onCancel={handleCancel}
+        >
+          <h1 className="add__teacher-title">O'qituvchi qo'shing</h1>
+          <form className="add__form" action="#">
+            <input className="add__form-input" type="text" placeholder="O'qituvchining ism familiyasi"/>
+            <input className="add__form-input" type="text" placeholder="O'qituvchining mutaxassisligi"/>
+            <input className="add__form-input" type="text" placeholder="O'qituvchining tajribasi (yillik)"/>
+          </form>
+        </Modal>
+      </div>
+    </>
+  );
+};
+
+export default Teacher;
